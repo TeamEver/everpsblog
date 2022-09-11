c19695938b1c36cb7149b321e2bbc88bca920d4a
cchalamon <cyril.chalamon@celaneo.com>
2022-09-11 17:11:51 +0200

    Upgrade version

--- everpsblog.php
+++ everpsblog.php
@@ -46,7 +46,7 @@ class EverPsBlog extends Module
     {
         $this->name = 'everpsblog';
         $this->tab = 'front_office_features';
-        $this->version = '5.3.25';
+        $this->version = '5.3.26';
         $this->author = 'Team Ever';
         $this->need_instance = 0;
         $this->bootstrap = true;
