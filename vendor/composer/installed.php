c61c88d6c4f19d33b8a3253f4ad3eefebb553a92
Cyssoo <cyrilchalamon@gmail.com>
2025-11-21 11:10:14 +0100

    Remove prestashop/module-lib-service dependency
    Deleted the 'prestashop/module-lib-service' package from composer.json and updated Composer autoload and metadata files accordingly. This change removes the dependency and regenerates Composer files to reflect the new package state.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'team-ever/everpsblog',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'prestashop-module',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'team-ever/everpsblog' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'prestashop-module',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
