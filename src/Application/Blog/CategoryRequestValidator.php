c2059fdf879649097e09ad7a7c50ca80961a1629
Cyssoo <cyrilchalamon@gmail.com>
2026-04-17 15:38:56 +0200

    Add structured blog request validation rules

--- src/Application/Blog/CategoryRequestValidator.php
+++ src/Application/Blog/CategoryRequestValidator.php
@@ -2,10 +2,22 @@
 
 namespace PrestaShop\Module\Everpsblog\Application\Blog;
 
-class CategoryRequestValidator
+class CategoryRequestValidator extends AbstractRequestValidator
 {
     public function validate(array $requestData): array
     {
+        $this->resetErrors();
+
+        $this->ensureDefaultTitle($requestData);
+        $requestData = $this->normalizeSeoFields($requestData);
+
+        $parentCategoryId = (int) ($requestData['id_parent_category'] ?? 0);
+        if ($parentCategoryId > 0 && !$this->existsInModuleTable('ever_blog_category', 'id_ever_category', $parentCategoryId)) {
+            $this->addFieldError('id_parent_category', sprintf('Catégorie parente introuvable (id: %d).', $parentCategoryId));
+        }
+
+        $this->throwIfInvalid();
+
         return $requestData;
     }
 }
--- src/Application/Blog/TagRequestValidator.php
+++ src/Application/Blog/TagRequestValidator.php
@@ -2,10 +2,17 @@
 
 namespace PrestaShop\Module\Everpsblog\Application\Blog;
 
-class TagRequestValidator
+class TagRequestValidator extends AbstractRequestValidator
 {
     public function validate(array $requestData): array
     {
+        $this->resetErrors();
+
+        $this->ensureDefaultTitle($requestData);
+        $requestData = $this->normalizeSeoFields($requestData);
+
+        $this->throwIfInvalid();
+
         return $requestData;
     }
 }
