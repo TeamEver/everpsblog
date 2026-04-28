9f89e40e652db0a35be16bbb206df8a5da2996d3
Cyssoo <cyrilchalamon@gmail.com>
2026-04-28 09:00:10 +0200

    Bump module version to 7.0.0
    Update EverPsBlog module version from 6.0.10 to 7.0.0 in everpsblog.php. This is a metadata-only change to reflect a new release/compatibility update.

--- everpsblog.php
+++ everpsblog.php
@@ -47,7 +47,7 @@ class EverPsBlog extends Module
     {
         $this->name = 'everpsblog';
         $this->tab = 'front_office_features';
-        $this->version = '6.0.10';
+        $this->version = '7.0.0';
         $this->author = 'Team Ever';
         $this->need_instance = 0;
         $this->bootstrap = true;
